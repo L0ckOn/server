1ab7d129f3272138e1d9daf100a3c5ca25942859
Tamik <gabaraev.tn@gmail.com>
2022-06-06 16:50:36 +0300

    first version of routes done

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,35 +3,56 @@ const router = express.Router();
 const tasks = require('./data.json');
 const fs = require('fs');
 
-router.get('/order=asc&page=1', (req, res) => {
-    const page = req.url[req.url.length - 1];
+router.get('/filterBy=&order=desc&page=:page', (req, res) => {
+    const page = req.params
     const startIndex = 5 * (page - 1);
     res.send(tasks[startIndex, startIndex + 5])
     
 });
 
-router.get('/order=asc&filterBy=done&page=1', (req, res) => {
+router.get('/filterBy=done&order=desc&page=:page', (req, res) => {
     const posts = tasks.filter(task => task.done)
-    const page = req.url[req.url.length - 1];
+    const page = req.params.page
     const startIndex = 5 * (page - 1);
     res.send(posts[startIndex, startIndex + 5])
     
 });
 
-router.get('/order=asc&filterBy=undone&page=1', (req, res) => {
+router.get('/filterBy=undone&order=desc&page=:page', (req, res) => {
     const posts = tasks.filter(task => !task.done)
+    const page = req.params.page
+    const startIndex = 5 * (page - 1);
+    res.send(posts[startIndex, startIndex + 5])
+    
+});
+
+router.get('/filterBy=&order=asc&page=:page', (req, res) => {
+    const posts = tasks.reverse()
     const page = req.url[req.url.length - 1];
     const startIndex = 5 * (page - 1);
     res.send(posts[startIndex, startIndex + 5])
     
 });
 
-router.get('/order=asc&page=1', (req, res) => {
+router.get('/filterBy=done&order=asc&page=:page', (req, res) => {
+    const posts = tasks.reverse().filter(task => task.done);
     const page = req.url[req.url.length - 1];
     const startIndex = 5 * (page - 1);
-    res.send(tasks[startIndex, startIndex + 5])
+    res.send(posts[startIndex, startIndex + 5])
     
 });
+
+router.get('/filterBy=&order=asc&page=:page', (req, res) => {
+    const posts = tasks.reverse().filter(!task.done);
+    const page = req.url[req.url.length - 1];
+    const startIndex = 5 * (page - 1);
+    res.send(posts[startIndex, startIndex + 5])
+    
+});
+
+router.post('/', (req, res) => {
+    console.log(req)
+})
 
 router
     .route('/:uuid')
